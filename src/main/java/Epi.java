e663863d5b263c303c19e1e374c9ad9a96a4e39c
naymin-gif <tharnaymin028@gmail.com>
2026-08-20 09:32:24 +0800

    "Write some greeting, asking, and goodbye message"

--- src/main/java/Epi.java
+++ src/main/java/Epi.java
@@ -9,5 +9,8 @@ public class Epi {
                 + "        | |      \n"
                 + "        |_|      \n";
         System.out.println(banner);
+        System.out.println("Meowdy! I'm Epi");
+        System.out.println("Are you ready to tackle some purr-fectly good tasks today?");
+        System.out.println("Meow for now. See you later!");
     }
 }

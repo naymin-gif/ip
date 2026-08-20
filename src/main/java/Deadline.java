948e16903f7227e65db54466a5e6d73c023033cf
naymin-gif <tharnaymin028@gmail.com>
2026-08-20 16:25:07 +0800

    "Added Deadline class that extends Tesk"

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,12 @@
+public class Deadline extends Task {
+    protected String finishBy;
+    public Deadline(String description, String by) {
+        super(description);
+        this.finishBy = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + "(by: " + this.finishBy + ")";
+    }
+}

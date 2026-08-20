d152060ef2f922c1ff9a324b97c0db45beee2584
naymin-gif <tharnaymin028@gmail.com>
2026-08-20 16:25:50 +0800

    "Make the bot to handle all the logic for different tasks"

--- src/main/java/Epi.java
+++ src/main/java/Epi.java
@@ -19,31 +19,58 @@ public class Epi {
 
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
-            String[] commandParts = input.trim().split("\\s+");
-            if (input.equalsIgnoreCase("bye")) {
+            String[] commandParts = input.trim().split("\\s+", 2);
+            String command = commandParts[0].toLowerCase();
+            String argument = commandParts.length > 1? commandParts[1]: "";
+            if (command.equals("bye")) {
                 System.out.println("Meow for now. See you later!");
                 break;
-            } else if (input.equalsIgnoreCase("list")) {
+            } else if (command.equals("list")) {
                 System.out.println("     Behold, your endless mountain of chores:");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println("   " + (i + 1) + ". " + tasks[i].toString());
                 }
-            } else if (commandParts[0].equalsIgnoreCase("mark")) {
+            } else if (command.equals("mark")) {
                 int taskIdx = Integer.parseInt(commandParts[1]) - 1;
                 tasks[taskIdx].markAsDone();
                 System.out.println("     About time you finished something. I've marked it as done:");
                 System.out.println("       " + tasks[taskIdx].toString());
-            } else if (commandParts[0].equalsIgnoreCase("unmark")) {
+            } else if (command.equals("unmark")) {
                 int taskIdx = Integer.parseInt(commandParts[1]) - 1;
                 tasks[taskIdx].markAsUndone();
                 System.out.println("     Slacking off, are we? I've marked this as not done:");
                 System.out.println("       " + tasks[taskIdx].toString());
+            } else if (command.equals("todo")) {
+                String description = argument.trim();
+                tasks[taskCount] = new Todo(description);
+                System.out.println("     More work? Fine. I have added this task:");
+                System.out.println("       " + tasks[taskCount].toString());
+                taskCount++;
+                System.out.println("     Now you have " + taskCount + " tasks in the list.");
+            } else if (command.equals("deadline")) {
+                String[] deadlineParts = argument.split(" /by ", 2);
+                String description = deadlineParts[0].trim();
+                String by = deadlineParts[1].trim();
+                tasks[taskCount] = new Deadline(description, by);
+                System.out.println("     A deadline? Better not miss it. I have added this task:");
+                System.out.println("       " + tasks[taskCount].toString());
+                taskCount++;
+                System.out.println("     Now you have " + taskCount + " tasks in the list.");
+
+            } else if (command.equals("event")) {
+                String[] eventParts = argument.split(" /from ", 2);
+                String description = eventParts[0].trim();
+                String[] timeparts = eventParts[1].split(" /to ", 2);
+                String from = timeparts[0].trim();
+                String to = timeparts[1].trim();
+                tasks[taskCount] = new Event(description, from, to);
+                System.out.println("     An event? I hope there will be treats. I have added this task:");
+                System.out.println("       " + tasks[taskCount].toString());
+                taskCount++;
+                System.out.println("     Now you have " + taskCount + " tasks in the list.");
             }
             else {
-                tasks[taskCount] = new Task(input);
-
-                System.out.println("Ugh, fine. Added: " + input);
-                taskCount++;
+                System.out.println("     I do not understand what this mean, Human.");
             }
         }
         scanner.close();

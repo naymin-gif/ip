7e2be396303e7bd24e87caf03621497c3b644929
naymin-gif <tharnaymin028@gmail.com>
2026-08-30 14:51:11 +0800

    Make Epi the application coordinator
    Move application setup and command execution into Epi instance state with a constructor and run method. Keep main as a minimal application entry point coordinating ConsoleUi, Storage, Parser, and TaskList.

--- src/main/java/Epi.java
+++ src/main/java/Epi.java
@@ -1,11 +1,17 @@
 import java.time.DateTimeException;
 
 public class Epi {
-    public static void main(String[] args) {
-        ConsoleUi ui = new ConsoleUi();
-        TaskList tasks = new TaskList();
-        Storage storage = new Storage("./data/epi.txt");
-        Parser parser = new Parser();
+    private final ConsoleUi ui;
+    private final Storage storage;
+    private final Parser parser;
+    private TaskList tasks;
+
+    public Epi(String filePath) {
+        ui = new ConsoleUi();
+        storage = new Storage(filePath);
+        parser = new Parser();
+        storage.prepareFile();
+        tasks = storage.load();
         String banner = "  ______       _ \n"
                 + " |  ____|     (_)\n"
                 + " | |__   _ __  _ \n"
@@ -17,48 +23,29 @@ public class Epi {
         ui.showLine(banner);
         ui.showLine("Meowdy! I'm Epi");
         ui.showLine("Are you ready to tackle some purr-fectly good tasks today?");
+    }
 
-        storage.prepareFile();
-        tasks = storage.load();
-
+    public void run() {
         while (ui.hasNextLine()) {
             String input = ui.readLine();
-
             try {
                 String[] commandParts = parser.parseInput(input);
                 String command = commandParts[0].toLowerCase();
                 String argument = commandParts.length > 1 ? commandParts[1] : "";
-
                 if (command.equals("bye")) {
                     ui.showLine("Meow for now. See you later!");
                     break;
                 } else if (command.equals("list")) {
-                    if (tasks.size() == 0) {
-                        throw new EpiException("Purr! There is no task in your list");
-                    }
-                        ui.showLine("     Here is your pile of tasks:");
-                    for (int i = 0; i < tasks.size(); i++) {
-                        ui.showLine("   " + (i + 1) + ". " + tasks.get(i).toString());
-                    }
+                    if (tasks.size() == 0) throw new EpiException("Purr! There is no task in your list");
+                    ui.showLine("     Here is your pile of tasks:");
+                    for (int i = 0; i < tasks.size(); i++) ui.showLine("   " + (i + 1) + ". " + tasks.get(i));
                 } else if (command.equals("delete")) {
-                    if (argument.isEmpty()) {
-                        throw new EpiException("You need to give me a task number to delete, human.");
-                    }
-                    int taskIdx = Integer.parseInt(argument) - 1;
-                    if (tasks.size() == 0) {
-                        throw new EpiException("Cannot! You don't even have a single task");
-                    }
-                    if (taskIdx < 0 || taskIdx >= tasks.size()) {
-                        throw new EpiException("That task number doesn't exist in my memory!");
-                    }
+                    int taskIdx = parser.parseTaskIndex(argument, tasks);
                     tasks.delete(taskIdx);
                     ui.showLine("Noted, I'll remove that from the task pile");
-                    int totalTaskNow = tasks.size();
-                    ui.showLine("Now you have " + totalTaskNow + " tasks in the list");
-
+                    ui.showLine("Now you have " + tasks.size() + " tasks in the list");
                 } else if (command.equals("mark") || command.equals("unmark")) {
                     int taskIdx = parser.parseTaskIndex(argument, tasks);
-
                     if (command.equals("mark")) {
                         tasks.get(taskIdx).markAsDone();
                         ui.showLine("     About time you finished something. I've marked it as done:");
@@ -66,31 +53,20 @@ public class Epi {
                         tasks.get(taskIdx).markAsUndone();
                         ui.showLine("     Slacking off, are we? I've marked this as not done:");
                     }
-                    ui.showLine("       " + tasks.get(taskIdx).toString());
+                    ui.showLine("       " + tasks.get(taskIdx));
                 } else if (command.equals("todo")) {
                     tasks.add(parser.parseTodo(argument));
-                    ui.showLine("     More work? Fine. I have added this task:");
-                    ui.showLine("       " + tasks.get(tasks.size() - 1).toString());
-                    ui.showLine("     Now you have " + tasks.size() + " tasks in the list.");
+                    showAddedTask("     More work? Fine. I have added this task:");
                 } else if (command.equals("deadline")) {
                     tasks.add(parser.parseDeadline(argument));
-                    ui.showLine("     A deadline? Better not miss it. I have added this task:");
-                    ui.showLine("       " + tasks.get(tasks.size() - 1).toString());
-                    ui.showLine("     Now you have " + tasks.size() + " tasks in the list.");
-
+                    showAddedTask("     A deadline? Better not miss it. I have added this task:");
                 } else if (command.equals("event")) {
                     tasks.add(parser.parseEvent(argument));
-                    ui.showLine("     An event? I hope there will be treats. I have added this task:");
-                    ui.showLine("       " + tasks.get(tasks.size() - 1).toString());
-                    ui.showLine("     Now you have " + tasks.size() + " tasks in the list.");
+                    showAddedTask("     An event? I hope there will be treats. I have added this task:");
                 } else {
                     throw new EpiException("I do not understand what that means, Human.");
                 }
-
-                if (!command.equals("list")) {
-                    storage.save(tasks);
-                }
-
+                if (!command.equals("list")) storage.save(tasks);
             } catch (EpiException e) {
                 ui.showLine(e.getMessage());
             } catch (NumberFormatException e) {
@@ -100,5 +76,15 @@ public class Epi {
             }
         }
         ui.close();
+    }
+
+    private void showAddedTask(String message) {
+        ui.showLine(message);
+        ui.showLine("       " + tasks.get(tasks.size() - 1));
+        ui.showLine("     Now you have " + tasks.size() + " tasks in the list.");
+    }
+
+    public static void main(String[] args) {
+        new Epi("./data/epi.txt").run();
     }
 }
